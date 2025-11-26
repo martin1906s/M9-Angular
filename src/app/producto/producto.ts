c1adb471f82da4d028fcb275296b11d66db379e1
Tin <martin.simbana007@gmail.com>
2025-11-26 10:12:57 -0500

    reto 17

--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -1,13 +1,20 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'mi-producto',
   templateUrl: './producto.html',
   styleUrl: './producto.css',
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
 })
 export class ProductoComponent {
   public colorear: boolean = true;
   public mostrar: string = 'Si';
+  misStyles= {
+    'color': 'white',
+    'background-color': 'blue',
+    'font-size': '20px',
+    'padding': '10px',
+    'border-radius': '15px'
+  }
 }
